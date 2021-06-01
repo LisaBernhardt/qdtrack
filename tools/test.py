717af678aa57f31f893d4dc568e9bc95d9176f0e
LisaBernhardt <bernhardt.lisamarie@gmail.com>
2021-06-01 15:59:02 +0200

    Update test.py

--- tools/test.py
+++ tools/test.py
@@ -119,7 +119,7 @@ def main():
         shuffle=False)
 
     # build the model and load checkpoint
-    model = build_model(cfg.model, train_cfg=None, test_cfg=cfg.get('test_cfg'))
+    model = build_model(cfg.model, train_cfg=None, test_cfg=None)
     # fp16_cfg = cfg.get('fp16', None)
     # if fp16_cfg is not None:
     #     wrap_fp16_model(model)
